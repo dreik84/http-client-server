f94b155d62dea069b06837564956d62d5d88b3a8
dreik84 <dreik84@mail.ru>
2025-10-16 18:41:01 +0300

    Create HttpClientRunner.java

---
+++ src/main/java/org/example/HttpClientRunner.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+
+public class HttpClientRunner {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8082"))
+                .header("content-type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofFile(Path.of("src/main/resources/example.json")))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.headers());
+        System.out.println(response.body());
+    }
+}
